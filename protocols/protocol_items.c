1a597b76f352dc75f9ec599b73a8baa806b1e0b0
Billymazze <163669423+Billymazze@users.noreply.github.com>
2024-04-22 13:07:34 -0500

    Update protocol_items.c

--- protocols/protocol_items.c
+++ protocols/protocol_items.c
@@ -2,6 +2,7 @@
 
 const SubGhzProtocol* tpms_protocol_registry_items[] = {
     &tpms_protocol_schrader_gg4,
+    &tpms_protocol_SMD3MA4,
 };
 
 const SubGhzProtocolRegistry tpms_protocol_registry = {
